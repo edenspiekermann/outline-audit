f2290284476c3dcf8f5f27c355b8ddd6058a3368
Florian Goße <florian@essog.de>
2016-03-30 11:32:32 +0200

    add bookmarklet script

---
+++ bookmarklet.js
@@ -0,0 +1,7 @@
+(function (document, body, script) {
+    body = document.body;
+    script = document.createElement('script');
+    script.src = 'https://cdn.rawgit.com/edenspiekermann/outline-audit/master/index.js';
+    script.async = true;
+    body.appendChild(script);
+})(document);
