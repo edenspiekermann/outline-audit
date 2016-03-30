8ffb884a02552f6dbc9c9229d175ae73da900b58
Florian Goße <florian@essog.de>
2016-03-30 12:11:41 +0200

    remove bookmarklet.js

--- bookmarklet.js
+++
@@ -1,7 +0,0 @@
-(function (document, body, script) {
-    body = document.body;
-    script = document.createElement('script');
-    script.src = 'https://cdn.rawgit.com/edenspiekermann/outline-audit/master/index.js';
-    script.async = true;
-    body.appendChild(script);
-})(document);
